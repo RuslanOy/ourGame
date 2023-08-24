9d43cfb702797b7245a24685c0c843b346bbf3de
glebpolk <glebpolk@gmail.com>
2023-08-24 19:30:30 +0300

    ServerApiRouter

---
+++ server/routes/api/question.api.router.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Question, Theme } = require('../../db/models');
+
+//вывести все карточки вопросов
+router.get('/', async (req, res) => {
+  try {
+    const questions = await Question.findAll({ include: { model: Theme } });
+    res.json(questions);
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
