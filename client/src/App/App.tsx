9c85f885878cf9b23b6bdcf4e8c8f96469844bda
Ekaterina Bakhmeteva <ekaterina.kukhorevich@gmail.com>
2023-08-24 17:28:45 +0300

    this is base

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
+function App():JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/game" element={<GamePage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/registration" element={<RegistrationPage />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/redux/reducers/rootReducer.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  // users: usersReducer,
+  // games: gamesReducer,
+});
+
+export default rootReducer;
